bbd3c9baafdce9eaf2a42edeb7524a395ae0ae72
Guido Olivero <102196175+Guidoolivero@users.noreply.github.com>
2026-09-16 13:31:11 -0300

    Agrega ejercicios especiales JSON/modal y links en el menu principal

---
+++ Especiales/datosCompartidos.js
@@ -0,0 +1,25 @@
+var textoUnidadesDeMedida = '{"unidadesDeMedida": [' +
+'{"codUm":"KG","descripcionUm":"Kilogramos"},' +
+'{"codUm":"UNI","descripcionUm":"Unidad"},' +
+'{"codUm":"LT","descripcionUm":"Litros"},' +
+'{"codUm":"LAT","descripcionUm":"Lata"},' +
+'{"codUm":"CJ","descripcionUm":"Caja"},' +
+'{"codUm":"B10","descripcionUm":"Bolsa 10 kg"},' +
+'{"codUm":"B20","descripcionUm":"Bolsa 20 kg"},' +
+'{"codUm":"B30","descripcionUm":"Bolsa 30 kg"}' +
+']}';
+
+var textoJSONRenglones = '{"renglones": [' +
+'{"nroFactura":"A-0001-00000001","fechaFactura":"2025-06-15","codArticulo":"ALM00","descripcion":"Almendras peladas orig. Marruecos","codUm":"KG","cantidad":12,"precioUnitario":18500,"importeRenglon":222000},' +
+'{"nroFactura":"A-0001-00000001","fechaFactura":"2025-06-15","codArticulo":"ALM03","descripcion":"Almendras peladas orig. Tailandia","codUm":"KG","cantidad":8,"precioUnitario":19200,"importeRenglon":153600},' +
+'{"nroFactura":"A-0001-00000002","fechaFactura":"2025-06-18","codArticulo":"ATU00","descripcion":"Atun al agua orig. Ecuador","codUm":"LAT","cantidad":48,"precioUnitario":2100,"importeRenglon":100800},' +
+'{"nroFactura":"A-0001-00000002","fechaFactura":"2025-06-18","codArticulo":"ATU01","descripcion":"Atun en aceite orig. Ecuador","codUm":"LAT","cantidad":36,"precioUnitario":2450,"importeRenglon":88200},' +
+'{"nroFactura":"B-0001-00000015","fechaFactura":"2025-07-02","codArticulo":"CHA00","descripcion":"Champignon en aceite","codUm":"LT","cantidad":20,"precioUnitario":3200,"importeRenglon":64000},' +
+'{"nroFactura":"B-0001-00000015","fechaFactura":"2025-07-02","codArticulo":"CHO01","descripcion":"Choclo orig. Ecuador","codUm":"LAT","cantidad":24,"precioUnitario":1800,"importeRenglon":43200},' +
+'{"nroFactura":"A-0001-00000020","fechaFactura":"2025-07-10","codArticulo":"PER00","descripcion":"Peras en almíbar Mendoza","codUm":"LT","cantidad":15,"precioUnitario":4100,"importeRenglon":61500},' +
+'{"nroFactura":"A-0001-00000020","fechaFactura":"2025-07-10","codArticulo":"PIM00","descripcion":"Pimienta blanca en grano","codUm":"KG","cantidad":6,"precioUnitario":9800,"importeRenglon":58800},' +
+'{"nroFactura":"A-0001-00000021","fechaFactura":"2025-07-22","codArticulo":"PIM01","descripcion":"Pimienta negra en grano","codUm":"KG","cantidad":10,"precioUnitario":8700,"importeRenglon":87000},' +
+'{"nroFactura":"A-0001-00000021","fechaFactura":"2025-07-22","codArticulo":"PIM03","descripcion":"Pimienta blanca molida","codUm":"KG","cantidad":5,"precioUnitario":11200,"importeRenglon":56000},' +
+'{"nroFactura":"C-0002-00000008","fechaFactura":"2025-08-04","codArticulo":"ARR10","descripcion":"Arroz largo fino bolsa","codUm":"B10","cantidad":40,"precioUnitario":6500,"importeRenglon":260000},' +
+'{"nroFactura":"C-0002-00000008","fechaFactura":"2025-08-04","codArticulo":"AZU20","descripcion":"Azúcar común bolsa","codUm":"B20","cantidad":18,"precioUnitario":8900,"importeRenglon":160200}' +
+']}';
